49cdeb13ec19325e8ad4da1943e34bda6b45e8ac
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-04 19:06:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)[0]
-  puts assign_rooms(attendees)[0]
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 
 end
