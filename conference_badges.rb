53da87800707e931a73eae3369c1eb886b64e125
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-04 18:59:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,8 @@ def assign_rooms(attendees)
   attendees.each_with_index {|name, idx| rooms << "Hello, #{name}! You'll be assigned to room #{idx + 1}!"}
   return rooms
 end
+
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+end
